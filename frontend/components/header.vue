6a953b84e1712af8c42980a2c2282e5d46743218
Youri Novikov <73738882+0youri@users.noreply.github.com>
2024-11-15 09:18:05 +0100

    create Header + Footer

---
+++ frontend/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-xl text-center px-2 pt-10">
+    { forge: body }
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-dark text-light cursor-pointer">
+    <Header />
+    <div class="h-screen flex flex-col">
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
